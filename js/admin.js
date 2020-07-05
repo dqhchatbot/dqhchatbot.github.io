061bb6af3aee3cc580f5debab20ba7cc9e4b56b4
DQH Chatbot <67799551+dqhchatbot@users.noreply.github.com>
2020-07-05 22:37:53 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -167,7 +167,7 @@ function uinfo(id) {
           redirectToLogin();
         } else {
           $('#ptnkinfo').html(`<b>ID: ${id}</b><br>Couldn't get info for user ${id}<br>
-                              <button class="btn btn-danger" onclick="removeUsr('2074201339276126','1259415580831106','MALE','MALE')">End chat</button>`);
+                              <button class="btn btn-danger" onclick="removeUsr(${id})">End chat</button>`);
         }
         return;
       }
@@ -176,19 +176,19 @@ function uinfo(id) {
 
       $('#ptnkinfo').html(`<b>ID: ${id}</b><br>${data.name} (<i>${data.gender == 'male' ? 'Nam' : 'Nữ'}</i>)<br>
           <img src="${data.profile_pic}" width="100px"/><br>
-          <button class="btn btn-danger" onclick="removeUsr('2074201339276126','1259415580831106','MALE','MALE')">End chat</button>`);
+          <button class="btn btn-danger" onclick="removeUsr(${id})">End chat</button>`);
     },
     errHandler
   );
 }
 
-function removeUsr(id1,id2,gender1,gender2) {
+function removeUsr(id) {
   var cf = confirm('Bạn có chắc muốn end chat người này?');
   if (cf)
     makeRequest(
       '/admin/edit/chatroom',
       'post',
-      { id1: id1,id2: id2,gender1: gender1,gender2: gender2, type: 'match' },
+      { id: id, type: 'remove' },
       function (res) {
         if (res.status === true) {
           $('#ptnkinfo').html('Ended chat for ID ' + id);
